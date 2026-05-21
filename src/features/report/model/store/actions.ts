83398565d5ba977c4ad39b071d1d7d5234060c41
Chlorine355 <akimoff.an@gmail.com>
2026-05-21 21:04:12 +0300

    fix: sending gosnomer

--- src/features/report/model/store/actions.ts
+++ src/features/report/model/store/actions.ts
@@ -15,7 +15,7 @@ export const sendReportFx = createEffect(async (report: ReportDataType) => {
         violation: report.violation!,
         datetime: report.date!.toISOString(),
         description: report.description!,
-        gosnomer: report.description!,
+        gosnomer: report.gosnomer!,
         lat: report.coords!.lat,
         lon: report.coords!.lon,
         agree: report.agree,
